dd0ccac4791c18054972896dc639d8b9860dbe35
BektashBey <93650793+BektashBey@users.noreply.github.com>
2024-07-11 16:53:29 +0300

    Add files via upload

---
+++ gun07/db_islemleri.py
@@ -0,0 +1,28 @@
+from orm_db import *
+
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def birimListele():
+    birimler = session.query(Birim).all()
+    for birim in birimler:
+        print(birim.birim_id, birim.birim_adi)
+    return birimler
+
+def birimEkle():
+    birimadi = input("bir birim adi giriniz")
+    yeniBirim = Birim(birim_adi=birimadi)
+    session.add(yeniBirim)
+    session.commit()
+
+def birimGuncelle():
+    birimListele()
+    birimid = int(input("Güncellemek istediğiniz birimin idsi giriniz"))
+    birim = session.query(Birim).filter(Birim.birim_id == birimid).first()
+    yeni_birim_adi = input("Yeni birim adini giriniz")
+    birim.birim_adi = yeni_birim_adi
+    session.commit()
+    return birimListele()
+
+birimGuncelle()
---
+++ gun08/app.py
@@ -0,0 +1,27 @@
+from flask import render_template
+from orm_db import *
+
+
+@app.route('/')
+def index():  # put application's code here
+    mesaj = "Merhaba Dünya"
+    try:
+        kullanici = User.query.filter(User.name=="esma", User.password=="nur").first()
+        # kullanici = User.query.filter_by(name=="esma", password=="nur").first()
+        print(kullanici)
+        if kullanici == None:
+            mesaj = "Kullanici bulunamadı"
+        else:
+            mesaj = "hoş geldiniz"+ kullanici.name
+    except:
+        mesaj =" sorgu hatası"
+    return render_template('index.html', mesaj = mesaj)
+
+@app.route('/yonetici')
+def yonetici():  # put application's code here
+    return 'Yönetici Sayfası'
+
+
+
+if __name__ == '__main__':
+    app.run()
